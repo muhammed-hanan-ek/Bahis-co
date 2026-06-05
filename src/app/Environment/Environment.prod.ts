6c9bdca8dc26e6edf188115ee83e735c4ebd119c
muhammedhanan9000 <mhdhananiqbal@gmail.com>
2026-06-05 14:42:47 +0530

    title changed as kaboom

--- src/app/Environment/Environment.prod.ts
+++ src/app/Environment/Environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  // ApiUrl: 'http://www.kaboomapp.in/apiKaboom',
-  ApiUrl: 'http://localhost:3000',
+  ApiUrl: 'http://www.kaboomapp.in/apiKaboom',
+  // ApiUrl: 'http://localhost:3000',
   hostUrl: 'http://localhost:4200',
 };
